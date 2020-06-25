44dfed1e142d423cc27bc8adbbabb0602da0496a
K-Resurreccion <sid@koolaru.com>
2020-06-25 11:05:49 +1000

    Add Handlebars to project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+console.log(document.getElementById('card').innerHTML);
+const source = document.getElementById('card').innerHTML;
+const template = Handlebars.compile(source);
 
+const tarotCards = {
+  cards: [
+    {name: 'The Fool',
+      img: './images/cards/major (0).jpg'},
+    {name: 'card two',
+      color: 'red'}]
+}
+
+const compiledHtml = template(tarotCards);
+const fill = document.getElementById('cardBox');
+console.log(fill);
+fill.innerHTML = compiledHtml;
+console.log(fill.innerHTML)
